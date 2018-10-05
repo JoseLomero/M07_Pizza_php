6ab73ca665460f8b21335001943e7fdaaba303fe
JoseLomero <jlomerooro@iesesteveterradas.cat>
2018-10-05 19:03:10 +0200

    primera version

---
+++ dades.php
@@ -0,0 +1,15 @@
+<?php
+  //variable global amb tots els ingredients que
+  //pot portar una pizza amb el seu codi.
+  $possibles_ingredients = array(
+      0 => 'Massa',
+      1 => 'Orenga',
+      2 => 'Formatge',
+      3 => 'Pernil dolç',
+      4 => 'Bacon',
+      5 => 'Piña'
+  );
+?>
+
+
+<input type="checkbox" name="ingredients['Massa', 'Orenga', 'Formatge', 'Pernil dolç', 'Bacon', Piña]">;
